b2f66cdd4fc945c71210bc5537a11fb669fbf9fe
Alireza Daneshvar <alirezad126@gmail.com>
2024-12-10 17:12:58 -0500

    finished building the environment for SimpleBuildingEnv

--- examples/domain_examples/rl_example/env.py
+++ examples/domain_examples/rl_example/env.py
@@ -18,49 +18,71 @@ class SimpleBuildingEnv(gym.Env):
 
         self.simulator = simulator if isinstance(simulator, LinearBuildingEnvelope) else psl.systems['LinearSimpleSingleZone'](seed=random_seed)
 
-        self.action_space = spaces.Box(-np.inf, np.inf, shape=self.simulator.get_U(1).shape[-1], dtype=np.float32)
-        self.observation_space = spaces.Box(-np.inf, np.inf, shape=self.simulator.x0.shape, dtype=np.float32)
+        self.action_space = spaces.Box(low=-np.inf,
+                                       high=np.inf,
+                                       dtype=np.float32,
+                                       shape=(self.simulator.get_U(2).shape[-1],))
+        self.observation_space = spaces.Box(low=-np.inf,
+                                            high=np.inf,
+                                            dtype=np.float32,
+                                            shape=(self.simulator.get_x0().shape[0] + self.simulator.get_D(1).shape[-1] + 2,))
 
         self.min_temp = min_temp
         self.max_temp = max_temp
         self.t = 0
-        self.A = torch.tensor(self.simulator.params[2]['A'])
-        self.B = torch.tensor(self.simulator.params[2]['Beta'])
-        self.C = torch.tensor(self.simulator.params[2]['C'])
-        self.E = torch.tensor(self.simulator.params[2]['E'])
-
         self.U_min = torch.tensor(self.simulator.umin)
         self.U_max = torch.tensor(self.simulator.umax)
-
-
-        obs = self.reset()
+        self.prev_action = None
 
     def build_state(self):
-        return [self.x, self.min_temp, self.max_temp, self.d]
+        observations =  [self.x, self.min_temp, self.max_temp, self.d]
+
+        combined_obs = []
+        for o in observations:
+            if isinstance(o, np.ndarray):  # Flatten if it's a NumPy array
+                combined_obs.extend(o.ravel())
+            else:  # Keep `None` as-is
+                combined_obs.append(o)
+
+        return combined_obs
 
     def step(self, action, D):
         self.t += 1
-        sim = self.simulator(nsim=1, x0=self.x, U=action, D=self.d)
-        self.x = sim["X"].squeeze()
-        self.y = sim["Y"].squeeze()
+        sim = self.simulator.simulate(nsim=1, x0=self.x, U=action, D=self.d)
+        self.x = sim["X"].reshape(self.simulator.get_x0().shape[0],)
+        self.y = sim["Y"]
         self.d = D if D is not None else self.simulator.get_D(1)
-        reward, cost = self.get_reward_cost(action, self.y)
+        reward = self.get_reward(action)
+        cost = self.get_cost(self.y)
+        self.prev_action = action
         next_obs = self.build_state()
-        return
+        return next_obs, reward, cost
 
     def reset(self):
-        self.simulator = psl.systems['LinearBuildingEnvelope'](seed=np.random.randint(1))
+        self.simulator = psl.systems['LinearSimpleSingleZone'](seed=np.random.randint(1000))
         self.t = 0
         self.x = self.simulator.get_x0()
-        self.d = self.simulator.get_d(1)
+        self.d = self.simulator.get_D(1)
         obs = self.build_state()
+
         return obs
+        # Combine all elements into a single list
 
-    def get_reward_cost(self, action, output):
 
-        return
+    def get_reward(self, action):
+        if self.prev_action is None:
+            reward = - (action * 0.01)
+        else:
+            reward = - (action * 0.01) - abs(action - self.prev_action) * 0.1
+        return reward
+
+    def get_cost(self, output):
+        cost = np.maximum(self.min_temp - output, 0) + np.maximum(output - self.max_temp, 0)
+        return cost
 
 
 
 if __name__ == "__main__":
-    env = SimpleBuildingEnv()
+    env = SimpleBuildingEnv(min_temp=20, max_temp=30)
+    obs = env.reset()
+    print(obs)
